e9e1dfe86cae1e958c0c5c4d0a889f92a23b66cb
Chris Eve Parravicini <chris.parravicini@gmail.com>
2023-01-19 21:25:37 -0300

    no funciona el signup/signin

--- src/store/auth.slice.js
+++ src/store/auth.slice.js
@@ -1,7 +1,32 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
+import User from "../models/user";
 import { URL_AUTH_SIGN_UP, URL_AUTH_SIGN_IN } from '../constants/firebase';
 
-export const signUp = createAsyncThunk('auth/signUp', async (email, password) => {
+
+const initialState = {
+    users: [],
+}
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState,
+    reducers: {
+        newUser: (state, action) => {
+            const newUser = new User (
+                action.payload.email,
+                action.payload.password, 
+            );
+            state.users.push(newUser);
+        },
+    },  
+});
+
+export const {newUser} = authSlice.actions;
+
+
+export const signUp = ({email, password}) => {
+    return async (dispatch) => {
+
     try {
         const response = await fetch(URL_AUTH_SIGN_UP, {
             method: 'POST',
@@ -19,13 +44,17 @@ export const signUp = createAsyncThunk('auth/signUp', async (email, password) =>
         }
 
         const data = await response.json();
-        return data;
+        // return data;
+        dispatch(newUser(data));
+        
     } catch (error) {
         throw error;
+        }
     }
-});
+};
 
-export const signIn = createAsyncThunk('auth/signIn', async (email, password) => {
+export const signIn = ({email, password}) => {
+    return async (dispatch) => {
     try {
         const response = await fetch(URL_AUTH_SIGN_IN, {
             method: 'POST',
@@ -40,36 +69,14 @@ export const signIn = createAsyncThunk('auth/signIn', async (email, password) =>
         });
 
         const data = await response.json();
-        return data;
+        // return data;
+        dispatch(newUser(data));
+
     } catch (error) {
         throw error;
+        }
     }
-});
+};
 
-const authSlice = createSlice({
-    name: 'auth',
-    initialState: { email: null, password: null },
-    reducers: {},
-    extraReducers: (builder) => {
-        builder
-            .addCase(signUp.pending, (state) => {
-                state.email = null;
-                state.password = null;
-            })
-            .addCase(signUp.fulfilled, (state, action) => {
-                state.email = action.payload.idEmail;
-                state.password = action.payload.idPassword;
-            })
-            .addCase(signIn.pending, (state) => {
-                state.email = null;
-                state.password = null;
-            })
-            .addCase(signIn.fulfilled, (state, action) => {
-                state.email = action.payload.idEmail;
-                state.password = action.payload.idPassword;
-            });
-    }
-});
 
-export const { } = authSlice.actions;
 export default authSlice.reducer;
